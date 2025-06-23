450817e517a2dbb058242fa53b0df8826c395fe2
Edwin-Arb <smayich_edwin@mail.ru>
2025-06-23 20:35:44 +0300

    Edit spawn position a bonus

--- Constants.h
+++ Constants.h
@@ -1,10 +1,12 @@
 ﻿#pragma once
+
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 600;
+
 constexpr int BONUS_DROP_CHANCE_PERCENT = 100;	// 10% шанс выпадения бонуса
 constexpr int SPAWN_BRICK_STRONG_PERCENT = 10;	// 10% шанс появления на карте этого типа кирпича
 constexpr int SPAWN_BRICK_GLASS_PERCENT = 25;	// 25% шанс появления на карте этого типа кирпича
-constexpr float SCALE_GAME = 3.f;
+
 constexpr float BALL_SPEED = 1.f;
 constexpr float FIREBALL_SPEED = 1.5f;
 constexpr float BONUS_FALLING_SPEED = 100.f;
